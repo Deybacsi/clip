03a9ca34a21b395b0d3dceed1c1da29563ad77d3
Deybacsi <deyprof@gmail.com>
2022-12-12 03:03:11 +0100

    c64intros added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # clip
 Command Line Intro Player
+
+
+
+on windows:
+use mobaxterm to run the .sh files
+better vsync, no screen tearing
+prereq:
+apt-get install coreutils
+
+
+on linux:
+not tested
---
+++ getc64intros.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# populates a list.txt file in intros/C64 that contains a group name and a filename
+# list.txt will be used to start the prg-s with vice
+
+# download the full intro pack zip from intros.c64.org, extract it to intros/C64 folder
+
+
+IFS=''
+
+MYTMP="./temp"
+
+rm $MYTMP/gtemp.txt
+rm $MYTMP/g2temp.txt
+> $MYTMP/list.txt
+> intros/C64/list.txt
+
+for LETTER in 0 ; do
+#for LETTER in 0 {A..Z}; do
+    echo $LETTER
+
+    #GROUPS=$(curl -k https://index.hu)
+
+    curl -s -k https://intros.c64.org/frame.php?letter=$LETTER | grep "module=showintros&group=" > $MYTMP/gtemp.txt
+
+    cat $MYTMP/gtemp.txt | while read GROUP; do
+        LINK=$(echo $GROUP | cut -d '"' -f 2)
+        NAME=$(echo $GROUP | cut -d '>' -f 2 | cut -d '<' -f 1)
+        #echo $LINK
+        if [[ "$NAME" =~ ", The" ]]; then
+            NAME=$(echo $NAME | sed 's/, The//g')
+            NAME="The $NAME"
+        fi
+        echo 
+        echo "Group: $NAME"
+        sleep 1
+        curl -s -k https://intros.c64.org/$LINK | grep "module=showintro&iid=" > $MYTMP/g2temp.txt
+        cat $MYTMP/g2temp.txt | while read INTRO; do
+            FILE=$(echo $INTRO | cut -d "'" -f2 | sed 's/\/intro\///g')
+            echo "      Intro: $FILE"
+            #echo $INTRO
+
+            echo "$NAME;$FILE" >> $MYTMP/list.txt
+        done
+
+    done
+
+done
+
+echo "Sorting final list"
+cat $MYTMP/list.txt | sort -u > intros/C64/list.txt
+
+echo "All done!"
+
+
+# https://www.lemon64.com/forum/viewtopic.php?t=55750&sid=90ce8080e4a1abfae02e1b4d4e788144
+
+
+

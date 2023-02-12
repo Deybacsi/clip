b8a3438c5f0d6885918cb5ac5d389a1be41cf20d
Deybacsi <deyprof@gmail.com>
2023-02-12 16:30:42 +0100

    fixed infofile swipe in emukiller

--- emukiller.sh
+++ emukiller.sh
@@ -2,6 +2,8 @@
 
 CNT=120
 CNTFILE="intros/C64/counter.txt"
+INFOFILE="intros/C64/nowplaying.txt"
+
 
 while [ $CNT -ne 0 ];do
     echo -en "Next in\n"$CNT > $CNTFILE
@@ -10,5 +12,6 @@ while [ $CNT -ne 0 ];do
     sleep 1
 done
 > $CNTFILE
+> INFOFILE
 ./nircmd.exe win activate ititle "OBS" && sleep 1
 killall x64sc
